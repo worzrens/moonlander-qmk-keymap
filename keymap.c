269181dbc406cd6e53bd043a578ea0ab7c5fc51b
Edgar Shport <eshport@enlil.com>
2022-09-25 13:13:58 +0300

    Tooltips for main layer

--- keymap.c
+++ keymap.c
@@ -77,10 +77,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_moonlander(
     TD(DANCE_0),    KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           LALT(KC_LSHIFT),                                MEH_T(KC_EQUAL),KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           TD(DANCE_1),                                    TD(DANCE_4),    KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLASH,
-    MO(3),          KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           TD(DANCE_2),                                                                    TD(DANCE_5),    KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,
-    KC_LSHIFT,      KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_BSLASH,
-    KC_LCTRL,       KC_TRANSPARENT, KC_PSCREEN,     KC_LEFT,        KC_RIGHT,       TD(DANCE_3),                                                                                                    MT(MOD_LCTL, KC_ENTER),KC_DOWN,        KC_UP,          KC_LBRACKET,    KC_RBRACKET,    MT(MOD_LCTL, KC_ENTER),
-    KC_SPACE,       MT(MOD_RSFT, KC_DELETE),KC_LCTRL,                       MT(MOD_RALT, KC_TAB),LT(4,KC_BSPACE),MT(MOD_LSFT, KC_SPACE)
+    MO(3),          KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           TD(DANCE_2),                                    TD(DANCE_5),    KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,
+    KC_LSHIFT,      KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_BSLASH,
+                                                                                                                 // Thumb L         // Thumb R
+    KC_LCTRL,       KC_TRANSPARENT, KC_PSCREEN,     KC_LEFT,        KC_RIGHT,                                    TD(DANCE_3),    MT(MOD_LCTL, KC_ENTER),               KC_DOWN,        KC_UP,          KC_LBRACKET,    KC_RBRACKET,    MT(MOD_LCTL, KC_ENTER),
+                                                                            // Piano L1          // Piano L2         // Piano L3             // Piano R3         // Piano R2     // Piano R1
+                                                                             KC_SPACE,       MT(MOD_RSFT, KC_DELETE), KC_LCTRL,          MT(MOD_RALT, KC_TAB),LT(4,KC_BSPACE),MT(MOD_LSFT, KC_SPACE)
   ),
   [1] = LAYOUT_moonlander(
     KC_ESCAPE,      KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,
